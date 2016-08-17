ef235861a2a076116dbd597d47bc98d260374a2d
Charlie Cook <turingbombe@gmail.com>
2016-08-17 14:48:13 -0400

    form_tag readme

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,18 @@
 class PostsController < ApplicationController
+  
 
+  def index
+    @posts = Post.all
+  end
+
+  def new
+
+  end
+
+  def create
+    
+    @post = Post.create({title: params[:title], description: params[:description]})
+    # byebug
+    redirect_to posts_path(@post)
+  end
 end
